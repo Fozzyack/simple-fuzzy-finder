71f3890babaf3228b7927c49365490504ef4d205
Frasier <fsundra@gmail.com>
2025-04-06 15:18:20 +0800

    Trying to use ncurses to read input

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <ncurses.h>
+
 
 using namespace std;
 namespace fs = std::filesystem ;
+
+
+vector<string> create_search_list(const string &path) {
+    vector<string> res;
+    for (fs::directory_entry dir_entry: fs::recursive_directory_iterator(path, fs::directory_options::skip_permission_denied)) {
+        res.push_back(dir_entry.path());
+    }
+    return res;
+}
 
 int main(int argc, char * argv[]) {
     
@@ -18,15 +29,22 @@ int main(int argc, char * argv[]) {
         initial_path = argv[1];
     }
 
-    std::vector<std::string> all_files;
-    fs::path path(initial_path);
-        for( fs::directory_entry dir_entry : fs::recursive_directory_iterator (path, fs::directory_options::skip_permission_denied)) {
-            all_files.push_back(dir_entry.path());
-    }
+    vector<string> dirs = create_search_list(initial_path);
+    for (size_t i = 0; i < dirs.size(); i++) cout << dirs[i] << endl;
 
-    for(size_t i = 0; i < all_files.size(); i++) {
-        cout << "[" << i << "] " << all_files[i] << endl;
+    char ch;
+    string input;
+    initscr();              // Start curses mode
+    cbreak();               // Disable line buffering
+    noecho();               // Don't echo input
+    refresh();
+    while(true) {
+        ch = getch();
+        if(ch == 27) break;
+        input += (char)ch;
+        refresh();
     }
+    cout << input << endl;
 
     return 0;
 }
