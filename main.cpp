431f7d9d27da2f17ed5ff851c470d366834eb1a3
Frasier <fsundra@gmail.com>
2025-04-09 21:39:22 +0800

    Moving to a multihreaded and switching to a scoring algorithm

--- main.cpp
+++ main.cpp
@@ -1,162 +1,184 @@
-#include <cstdlib>
-#include <unistd.h>
-#include <iostream>
-#include <string>
 #include <algorithm>
-#include <vector>
+#include <cctype>
+#include <cstdlib>
 #include <filesystem>
+#include <iterator>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+
 #include <ncurses.h>
-#include <set>
+
 
 using namespace std;
 namespace fs = std::filesystem;
 
-
-/*
- * Returns a vector of all possible files
- * Could potential remove (duplicates) and not add it in if the file is a directory
- */
-
-vector<string> create_file_list(fs::path &path, bool dir_only) {
-    vector<string> res;
-    res.push_back(path.string());
-    set<string> seen;
-    for(fs::directory_entry dir_entry : fs::recursive_directory_iterator(path, fs::directory_options::skip_permission_denied)) {
-        if (dir_only) {
-            if (dir_entry.is_directory() && !seen.count(dir_entry.path())) {
-                seen.insert(dir_entry.path());
-                res.push_back(dir_entry.path());
-            }
-        }
-        else {
-            res.push_back(dir_entry.path());
-        }
-    }
-    return res;
-}
-
-/*
- * Provides the real time searching of data through the screen.
- * Displays the top LIST_SIZE results
- */
-
-string search_screen(vector<string> &dirs, string search, int &index) {
-
-    int LIST_SIZE = 35;
-
-    vector<string> print_dir;
-    string res;
-
-    copy_if(dirs.begin(), dirs.end(), std::back_inserter(print_dir),
-            [search](std::string &s) {
-            return !s.empty() && s.find(search) != string::npos;
-            });
-
-    size_t entry_length = min((int)print_dir.size(), LIST_SIZE);
-    if (print_dir.size() != 0) {
-        if(index < 0) index = entry_length - 1;
-        index = index % entry_length;
-    } else index = 0;
-
-    for (size_t i = 0; i < entry_length; i++) {
-        if (index == i) {
-            printw("[*] %s\n", print_dir[i].c_str());
-            res = print_dir[i];
-        } else {
-            printw("%s\n", print_dir[i].c_str());
-        }
-    }
-    printw("%d size of print_dir\n", (int)print_dir.size());
-    printw("%d size of index\n", index);
-    return res;
-}
+void get_dirs(const fs::path home_dir, vector<string> &dirs); 
+void render_list(const vector<string> &dirs, string &search, int &entries_to_display);
 
 int main(int argc, char *argv[]) {
 
     char *home = getenv("HOME");
-    fs::path path("./");
+    fs::path home_dir(home);
+    vector<string> dirs;
 
-    if (argc > 1) {
-        if (string(argv[1]) == "-s" || string(argv[1]) == "--start") path = string(home);
-        else if (string(argv[1]) == "--help" || string(argv[1]) == "-h") {
-            exit(5);
-        }
-        else path = argv[1];
-    }
+    thread make_dir_thread(get_dirs, home_dir, ref(dirs));
 
-
-    /*
-     * Creates a new screen. There is a problem if you try to print to stdout
-     */
-
-    SCREEN *s = NULL;
-    FILE* out = stdout;
+    SCREEN *s = NULL; FILE *out = stdout;
     if(!isatty(fileno(stdout))) {
         out = fopen("/dev/tty", "w");
         setbuf(out, NULL);
     }
     s = newterm(NULL, out, stdin);
 
+
+    initscr();
     cbreak();
-    noecho();
-    keypad(stdscr, true);
-    scrollok(stdscr, true);
+    scrollok(stdscr, TRUE);
+    keypad(stdscr, TRUE);
 
-    vector<string> dirs = create_file_list(path, false);
 
-    string search; //Holds the search string
-    string res_path; // Holds the currently selected path
-    int ch; // The character that the user enters
-    int index = 0; //The index of the list which the user has selected
+    int input;
+    int entries_to_display = 20;
+    string search = "";
 
-    /*
-     * The main loop that runs
-     */
-    while(ch != '\n') {
-        clear();
-        res_path = search_screen(dirs, search, index);
-        if (res_path != "") { // Print the users current selection
-            printw("Current Selection: %s\n", res_path.c_str());
-        }
-        printw("Search: %s", search.c_str());
-        refresh();
+    make_dir_thread.join();
+    render_list(dirs, search, entries_to_display);
+    printw("Search: %s", search.c_str());
 
-        ch = getch();
-        switch(ch) {
+    while(input != '\n') {
+        input = getch();
+        switch(input) {
             case KEY_BACKSPACE:
-            case KEY_DC:
-                if (search.size() != 0) search.pop_back();
+                if (search.size() > 0) search.pop_back();
                 break;
-            case KEY_UP:
-                index--;
-                break;
-            case KEY_STAB:
-            case '\t':
-            case KEY_DOWN:
-                index++;
-                break;
-            case KEY_LEFT:
+            case '\n':
+            case KEY_ENTER:
                 break;
             case KEY_RIGHT:
+                entries_to_display++;
+                break;
+            case KEY_LEFT:
+                if (entries_to_display > 0) entries_to_display--;
                 break;
             default:
-                search += ch;
-                index = 0;
+                search += input;
                 break;
         }
+
+        clear();
+
+        render_list(dirs, search, entries_to_display);
+        printw("Search: %s", search.c_str());
+        refresh();
     }
 
-    endwin();
-    if (res_path == "") {
-        cout << "No Option selected (returned an empty path)" << endl;
-        exit(0);
-    }
 
-    fs::path final(res_path);
-    if(!fs::is_directory(final)) {
-        final.remove_filename();
-    }
-    cout << fs::absolute(final).string() << endl;
 
     return 0;
 }
+
+void get_dirs(const fs::path home_dir, vector<string> &dirs) {
+
+    for (fs::directory_entry dir_entry : fs::recursive_directory_iterator(home_dir, fs::directory_options::skip_permission_denied)) {
+        dirs.push_back(dir_entry.path());
+    }
+
+}
+
+
+int char_bonus(char prev, char curr) {
+    if (prev == '/' || prev == '-' || prev == '_' || isspace(prev))
+        return 10;
+    return 0;
+}
+
+void fuzzy_match_score(const string &dir_entry, const string &search, int &res_score) {
+    size_t n = dir_entry.size(), m = search.size();
+    size_t i = 0, j = 0;
+    int score = 0;
+    int consequtive_bonus = 10;
+    int position_bonus = 5;
+    int prev_match = -1;
+
+    if (dir_entry == search) score += 1000;
+    if( dir_entry.find(search) != string::npos) score += 500;
+
+    while (i < n && j < m) {
+        if (islower(dir_entry[i]) == islower(search[j])) {
+            score += 100;
+            score += max(0, static_cast<int>(50 - i));
+
+            if(i > 0) {
+                char_bonus(dir_entry[i - 1], dir_entry[i]);
+            } else {
+                score += 10;
+            }
+            if (prev_match == static_cast<int>(i) - 1) {
+                score += consequtive_bonus;
+            }
+            prev_match = i;
+            ++j;
+        }
+        ++i;
+    }
+    score -= dir_entry.length();
+    res_score = score;
+
+}
+
+void thread_score(int start, int end, const vector<string> &dirs, const string &search, vector<pair<int, string>> &result, mutex &mu_check) {
+
+    vector<pair<int, string>> local;
+
+    for (size_t i = start; i < end; i++) {
+        int score;
+        fuzzy_match_score(dirs[i], search, score);
+        local.push_back({score, dirs[i]});
+    }
+
+    lock_guard<mutex> lock(mu_check);
+    result.insert(result.end(), local.begin(), local.end());
+}
+
+void render_list(const vector<string> &dirs, string &search, int &entries_to_display) {
+
+    int nu_display = entries_to_display;
+
+    if (search == "") {
+        if (dirs.size() <= nu_display) nu_display = dirs.size();
+        for (size_t i = 0; i < nu_display; i++) {
+            printw("%s\n", dirs[i].c_str());
+        }
+    } else {
+
+        size_t num_threads = thread::hardware_concurrency();
+        size_t chunk_size = (dirs.size() + num_threads - 1) / num_threads;
+        mutex mu_check;
+        vector<pair<int, string>> result;
+        vector<thread> threads;
+
+        for(int i = 0; i < num_threads; i++) {
+            size_t start = i * chunk_size;
+            size_t end = min(start + chunk_size, dirs.size());
+            threads.push_back(thread(thread_score, start, end, cref(dirs), cref(search), ref(result), ref(mu_check)));
+        }
+
+        for (thread &t: threads) t.join();
+
+        sort(result.begin(), result.end(),
+                [](const pair<int, string> a, const pair<int, string> b) {
+                    return a.first > b.first;
+                });
+
+        if (result.size() <= nu_display) nu_display = result.size();
+        for (size_t i = 0; i < nu_display; i++) {
+            printw("%d: %s\n", result[i].first, result[i].second.c_str());
+        }
+
+    }
+}
+
